5b3b5c77557a9f3b97a6023ef81bf67631a26d19
gschneeloch <gschneeloch@9d7a3aff-8e18-0410-9cb2-e11f6afa012d>
2013-10-29 20:02:26 +0000

    Fix pagination limit for all rows
    git-svn-id: svn+ssh://grizzly/var/svn/gfy_core/trunk/www/browser/lib/data_table@5756 9d7a3aff-8e18-0410-9cb2-e11f6afa012d

--- src/data_table_pagination.php
+++ src/data_table_pagination.php
@@ -254,7 +254,7 @@ class DataTablePaginationState {
 			$array = array(self::limit_key => null,
 				self::current_page_key => 0);
 		}
-		if (array_key_exists(self::limit_key, $array) && $array[self::limit_key]) {
+		if (array_key_exists(self::limit_key, $array)) {
 			$this->limit = (int)$array[self::limit_key];
 		}
 		else {
