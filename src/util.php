ff6c1f310f2f63e2d488af14ef093583d326a7b4
gschneeloch <gschneeloch@9d7a3aff-8e18-0410-9cb2-e11f6afa012d>
2013-11-06 17:49:46 +0000

    Add functions to create DataForm from SQL
    git-svn-id: svn+ssh://grizzly/var/svn/gfy_core/trunk/www/browser/lib/data_table@5800 9d7a3aff-8e18-0410-9cb2-e11f6afa012d

--- src/util.php
+++ src/util.php
@@ -5,7 +5,7 @@
 /**
  * Create some default datatable columns for a database table
  *
- * @param $res
+ * @param $res resource
  * @return DataTableColumn[]
  * @throws Exception
  */
@@ -25,9 +25,75 @@ function create_columns_from_database($res) {
 		/** @var string $field_name */
 		$field_name = $field->name;
 
-		$column = DataTableColumnBuilder::create()->display_header_name(ucwords($field_name))->column_key($field_name)->build();
+		$column = DataTableColumnBuilder::create()->display_header_name(ucwords($field_name))->column_key($field_name)->sortable(true)->build();
 		$columns[] = $column;
 	}
 
 	return $columns;
+}
+
+/**
+ * @param $res resource
+ * @return array
+ * @throws Exception
+ */
+function get_rows_from_database($res) {
+	if (!is_resource($res)) {
+		throw new Exception("res is not an open database connection");
+	}
+
+	$rows = array();
+	while ($row = gfy_db::fetch_assoc($res)) {
+		$rows[] = $row;
+	}
+	return $rows;
+}
+
+function create_table_from_database($sql, $state, $submit_url=null) {
+	if (!$sql || !is_string($sql)) {
+		throw new Exception("sql must be a string of SQL");
+	}
+	if (!$state || !($state instanceof DataFormState)) {
+		throw new Exception("state must exist and must be instanceof DataFormState");
+	}
+	if ($submit_url && !is_string($submit_url)) {
+		throw new Exception("submit must be a URL");
+	}
+
+	$count_sql = SQLBuilder::create($sql)->state($state)->build_count();
+	$count_res = gfy_db::query($count_sql, null, true);
+	$count_row = gfy_db::fetch_row($count_res);
+	$num_rows = (int)$count_row[0];
+
+	$settings = DataTableSettingsBuilder::create()->total_rows($num_rows)->build();
+	$paginated_sql = SQLBuilder::create($sql)->settings($settings)->state($state)->build();
+
+	$columns = create_columns_from_database($paginated_sql, $state);
+	$rows = get_rows_from_database($paginated_sql, $state);
+
+	$widgets = array();
+	if ($submit_url) {
+		$button = DataTableButtonBuilder::create()->text("Submit")->behavior(new DataTableBehaviorSubmit())->form_action($submit_url)->build();
+		$widgets[] = $button;
+	}
+
+	$table = DataTableBuilder::create()->rows($rows)->columns($columns)->widgets($widgets)->build();
+	return $table;
+}
+
+/**
+ * @param $sql string
+ * @param $state DataFormState
+ * @return DataForm
+ * @throws Exception
+ */
+function create_data_form_from_database($sql, $state) {
+	if (!$sql || !is_string($sql)) {
+		throw new Exception("sql must be a string of SQL");
+	}
+
+	$table = create_table_from_database($sql, $state);
+
+	$form = DataFormBuilder::create($state->get_form_name())->remote($_SERVER['REQUEST_URI'])->tables(array($table))->build();
+	return $form;
 }
