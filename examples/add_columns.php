eeae5c4760a256e2cd12b3d5e02fd0df7593d28a
gschneeloch <gschneeloch@9d7a3aff-8e18-0410-9cb2-e11f6afa012d>
2013-12-12 20:18:03 +0000

    Code cleanup and explanation
    git-svn-id: svn+ssh://grizzly/var/svn/gfy_core/trunk/www/browser/lib/data_table@5918 9d7a3aff-8e18-0410-9cb2-e11f6afa012d

--- examples/add_columns.php
+++ examples/add_columns.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * Example for adding columns to a DataForm via AJAX
+ *
  * LICENSE: This source file and any compiled code are the property of its
  * respective author(s).  All Rights Reserved.  Unauthorized use is prohibited.
  *
@@ -13,19 +15,25 @@ require_once "../../../../../lib/main_lib.php";
 require_once FILE_BASE_PATH . "/www/browser/lib/data_table/data_form.php";
 
 /**
- * @param DataFormState $state
+ * Create DataForm for example
+ *
+ * @param DataFormState $state State of form which roughly encapsulates $_POST
  * @return DataForm
  */
 function make_form($state) {
 	$this_url = $_SERVER['REQUEST_URI'];
 
+	// numbers in $rows will be converted to checkboxes using this formatter
 	$checkbox_formatter = new DataTableCheckboxCellFormatter();
 
+	// create first three columns
 	$columns = array();
 	$columns[] = DataTableColumnBuilder::create()->display_header_name("Select")->column_key("perm_1")->cell_formatter($checkbox_formatter)->build();
 	$columns[] = DataTableColumnBuilder::create()->display_header_name("Permanent Column #1")->column_key("perm_1")->build();
 	$columns[] = DataTableColumnBuilder::create()->display_header_name("Permanent Column #2")->column_key("perm_2")->build();
 
+	// The Add Column and Remove Column buttons just refresh the table while
+	// updating this parameter
 	$num_columns = $state->find_item(array("num_columns"));
 	if (is_null($num_columns)) {
 		$num_columns = 2;
@@ -35,39 +43,48 @@ function make_form($state) {
 		$num_columns = (int)$num_columns;
 	}
 
-	for ($j = 2; $j < $num_columns; $j++) {
-		$columns[] = DataTableColumnBuilder::create()->display_header_name("Generated Column #" . ($j - 1))->column_key("gen_" . ($j - 1))->build();
+	// create all generated columns
+	for ($col_num = 2; $col_num < $num_columns; $col_num++) {
+		$columns[] = DataTableColumnBuilder::create()->display_header_name("Generated Column #" . ($col_num - 1))->column_key("gen_" . ($col_num - 1))->build();
 	}
 
 	// keep value deterministic so numbers don't change between refreshes
 	srand(0);
 
-	$rows = array();
-	for ($i = 0; $i < 15; $i++) {
-		$row = array();
-		$row["perm_1"] = $i;
-		$row["perm_2"] = rand();
-		for ($j = 2; $j < $num_columns; $j++) {
-			$row["gen_" . ($j-1)] = rand();
-		}
+	$num_rows = 15;
 
-		$rows[] = $row;
+	// generate data to display in table.
+	$rows = array();
+	for ($row_num = 0; $row_num < $num_rows; $row_num++) {
+		// write out permanent columns
+		$rows[$row_num] = array();
+		$rows[$row_num]["perm_1"] = $row_num;
+		$rows[$row_num]["perm_2"] = rand();
+	}
+	for ($col_num = 2; $col_num < $num_columns; $col_num++) {
+		for ($row_num = 0; $row_num < $num_rows; $row_num++) {
+			// Each key gen_0, gen_1, etc corresponds to the DataTableColumn objects we created earlier
+			$rows[$row_num]["gen_" . ($col_num-1)] = rand();
+		}
 	}
 
 	$form_name = $state->get_form_name();
 
+	// add Add Column and Remove Column buttons
 	$widgets = array();
 
 	$num_columns_name = DataFormState::make_field_name($form_name, array("num_columns"));
 	$add_column_behavior = new DataTableBehaviorRefresh(array($num_columns_name => ($num_columns + 1)));
-	$widgets[] = DataTableButtonBuilder::create()->name("add_column")->text("Add Column")->form_action($this_url)->behavior($add_column_behavior)->build();
+	$widgets[] = DataTableButtonBuilder::create()->text("Add Column")->form_action($this_url)->behavior($add_column_behavior)->build();
 
 	if ($num_columns > 2) {
 		$remove_column_behavior = new DataTableBehaviorRefresh(array($num_columns_name => ($num_columns - 1)));
-		$widgets[] = DataTableButtonBuilder::create()->name("add_column")->text("Remove Column")->form_action($this_url)->behavior($remove_column_behavior)->build();
+		$widgets[] = DataTableButtonBuilder::create()->text("Remove Column")->form_action($this_url)->behavior($remove_column_behavior)->build();
 	}
 
+	// create DataTable from rows, buttons, and columns
 	$table = DataTableBuilder::create()->columns($columns)->rows($rows)->widgets($widgets)->build();
+	// create DataForm from DataTable
 	$form = DataFormBuilder::create($state->get_form_name())->tables(array($table))->remote($this_url)->build();
 	return $form;
 }
@@ -76,6 +93,7 @@ try {
 	$state = new DataFormState("add_columns", $_POST);
 	$form = make_form($state);
 	if ($state->only_display_form()) {
+		// Just asking for a refresh. Only display form HTML
 		echo $form->display_form($state);
 	}
 	else
