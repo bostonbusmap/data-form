7f16c40aee81bb0677a2eeb4b0f5804c02c353d4
gschneeloch <gschneeloch@9d7a3aff-8e18-0410-9cb2-e11f6afa012d>
2013-12-12 19:55:17 +0000

    Comments and code cleanup
    git-svn-id: svn+ssh://grizzly/var/svn/gfy_core/trunk/www/browser/lib/data_table@5915 9d7a3aff-8e18-0410-9cb2-e11f6afa012d

--- src/data_table_search_formatter.php
+++ src/data_table_search_formatter.php
@@ -6,6 +6,8 @@ require_once "util.php";
  */
 interface IDataTableSearchFormatter {
 	/**
+	 * Render HTML for the search control
+	 *
 	 * TODO: figure out a way to simplify this interface, remove some parameters
 	 *
 	 * @param $form_name string
@@ -13,17 +15,20 @@ interface IDataTableSearchFormatter {
 	 * @param $form_method string Either post or get
 	 * @param $table_name string Name of table if table has a name
 	 * @param $column_key string Which column search formatter applies to
-	 * @param $state DataFormState
+	 * @param $state DataFormState State of form
 	 * @param $default_value DataTableSearchState whatever the user filled in when the refresh happened
 	 * @param $label string HTML to display for the label for this element. May be empty string for no label
-	 * @return mixed
+	 * @return string
 	 */
 	function format($form_name, $form_action, $form_method, $table_name, $column_key, $state, $default_value, $label);
 }
 
+/**
+ * Renders a textbox for string searching
+ */
 class TextboxSearchFormatter implements IDataTableSearchFormatter {
 	/**
-	 * @var string
+	 * @var string Type of search
 	 */
 	protected $type;
 	public function __construct($type) {
@@ -51,16 +56,16 @@ class TextboxSearchFormatter implements IDataTableSearchFormatter {
 		$type_key = array_merge($searching_state_key, array(DataTableSearchState::type_key));
 		$params_key = array_merge($searching_state_key, array(DataTableSearchState::params_key, "0"));
 
-		$type_name = DataFormState::make_field_name($form_name, $type_key);
-
-		// TODO: replace with DataTableHidden
-		$ret = '<input type="hidden" name="' . htmlspecialchars($type_name) . '" value="' .
-			htmlspecialchars($default_type) . '" />';
+		$ret = DataTableHidden::display_hidden($form_name, $state, $type_key, $default_type);
 		$ret .= DataTableTextbox::display_textbox($form_name, $params_key, $form_action, $form_method, new DataTableBehaviorRefresh(), $default_param, $label, $state);
 
 		return $ret;
 	}
 }
+
+/**
+ * Renders a textbox for numerical filtering
+ */
 class NumericalSearchFormatter implements IDataTableSearchFormatter {
 	function format($form_name, $form_action, $form_method, $table_name, $column_key, $state, $default_value, $label)
 	{
