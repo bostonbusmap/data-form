3705337fba4e71a87fb6df34b417cbe6505e631f
gschneeloch <gschneeloch@9d7a3aff-8e18-0410-9cb2-e11f6afa012d>
2013-12-17 16:27:24 +0000

    Change SQL example to use organisms table to avoid user-specific results and unnecessary complexity
    git-svn-id: svn+ssh://grizzly/var/svn/gfy_core/trunk/www/browser/lib/data_table@5944 9d7a3aff-8e18-0410-9cb2-e11f6afa012d

--- examples/external_search.php
+++ examples/external_search.php
@@ -14,56 +14,8 @@ require_once "../../../../../lib/main_lib.php";
 
 require_once FILE_BASE_PATH . "/www/browser/lib/data_table/data_form.php";
 
-/**
- * Creates a SQL query to get searches based on the user currently logged in
- *
- * @return string SQL
- * @throws Exception
- */
-function make_external_search_query() {
-	verify_login();
+require_once "sql_lib.php";
 
-	$current_user = user::get_current_user();
-	if (!$current_user) {
-		throw new Exception("user is not logged in");
-	}
-	$user_id = (int)$current_user->get_id();
-	$browse_searches_query = "SELECT DISTINCT
-		s.search_perscan_table_name,
-		s.search_date,
-		s.run_id AS rid,
-		s.search_id,
-		s.search_name AS search_name,
-		sa.search_algorithm_type AS stype,
-		s.search_notes AS snotes,
-		sc.scans_name AS scans_name,
-		sc.scans_id AS scid,
-		r.run_name AS run_name,
-		r.run_status as run_status,
-		s.search_peptide_mass_units as mass_units,
-		a.access_type AS acc_type,
-		u.user_name,
-		GROUP_CONCAT(DISTINCT run_conn_table_type) AS tables
-	FROM
-		`access` AS a,
-		`scans` AS sc,
-		`runs` AS r,
-		`search_algorithm` AS sa,
-		users AS u,
-		`searches` AS s LEFT JOIN runs_connector AS rc ON s.run_id = rc.run_id
-	WHERE
-		a.user_id = $user_id AND
-		a.access_id = s.access_id
-		AND s.scans_id = sc.scans_id
-		AND u.user_id=a.user_id
-		AND sa.search_algorithm_id = s.search_algorithm_id
-		AND s.run_id = r.run_id
-		AND s.search_perscan_table_name IS NOT NULL
-		AND s.search_perhit_table_name IS NOT NULL
-	GROUP BY s.search_id
-";
-	return $browse_searches_query;
-}
 
 /**
  * Creates a DataForm object for this example
@@ -73,7 +25,7 @@ function make_external_search_query() {
  */
 function make_external_search_form($state) {
 	// generate some SQL
-	$browse_searches_query = make_external_search_query();
+	$browse_searches_query = make_organisms_query();
 
 	// SQLBuilder will parse the SQL (using PHP-SQL-Parser) and store it for future manipulation
 	$sql_builder = new SQLBuilder($browse_searches_query);
@@ -87,16 +39,16 @@ function make_external_search_form($state) {
 	$num_rows = (int)$row[0];
 
 	// Tell DataTable how many rows we have. This is needed for pagination.
-	$settings = DataTableSettingsBuilder::create()->total_rows($num_rows)->build();
+	$settings = DataTableSettingsBuilder::create()->total_rows($num_rows)->default_limit(10)->build();
 
 	// Create three columns: search_id which is checkboxes to select rows,
 	// search_date and search_name. Set search_date to be sortable so we can show off this feature
 	$columns = array();
-	$columns[] = DataTableColumnBuilder::create()->column_key("search_id")->
+	$columns[] = DataTableColumnBuilder::create()->column_key("organism_id")->
 		cell_formatter(new DataTableCheckboxCellFormatter())->
 		header_formatter(new DataTableCheckboxHeaderFormatter())->build();
-	$columns[] = DataTableColumnBuilder::create()->display_header_name("Date created")->column_key("search_date")->sortable(true)->build();
-	$columns[] = DataTableColumnBuilder::create()->display_header_name("Search name")->column_key("search_name")->build();
+	$columns[] = DataTableColumnBuilder::create()->display_header_name("Organism name")->column_key("organism_name")->sortable(true)->build();
+	$columns[] = DataTableColumnBuilder::create()->display_header_name("Scientific name")->column_key("organism_scientific")->build();
 
 	// Make a SQL query which takes into account pagination, filtering and sorting. This will be like our original
 	// query but with a LIMIT clause, the search text added in a WHERE clause, and maybe an ORDER BY clause.
@@ -105,12 +57,12 @@ function make_external_search_form($state) {
 	// Create the search textbox widget. Use search_type rlike to use regular expressions.
 	$widgets = array();
 
-	$widgets[] = DataTableSearchWidgetBuilder::create()->column_key("search_name")->search_type(DataTableSearchState::rlike)->
-		label("Search name:")->build();
+	$widgets[] = DataTableSearchWidgetBuilder::create()->column_key("organism_name")->search_type(DataTableSearchState::rlike)->
+		label("Search organism name: ")->build();
 
 
 
-	$table = DataTableBuilder::create()->columns($columns)->rows(new DatabaseIterator($query, null, "search_id"))->
+	$table = DataTableBuilder::create()->columns($columns)->rows(new DatabaseIterator($query, null, "organism_name"))->
 		settings($settings)->widgets($widgets)->build();
 	$form = DataFormBuilder::create($state->get_form_name())->remote("external_search.php")->
 		tables(array($table))->build();
@@ -119,7 +71,7 @@ function make_external_search_form($state) {
 
 try {
 	// $state contains our form state which contains search text among other things
-	$state = new DataFormState("searches", $_GET);
+	$state = new DataFormState("organisms", $_GET);
 	// create our DataForm
 	$form = make_external_search_form($state);
 	// If an AJAX request, just display the form HTML
